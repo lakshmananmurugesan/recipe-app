2a816639585479d7d4f3fdb8976d64618f788926
lakshman <lakshmanmurugesan@gmail.com>
2024-04-13 23:00:42 +0530

    Add express app

---
+++ api_app/api.js
@@ -0,0 +1,12 @@
+// server/server.js
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.get('/api/data', (req, res) => {
+  res.json({ message: 'Hello from the server!' });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});

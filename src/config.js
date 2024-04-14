450117a5863e67d1e3da7e940b74292b167842ed
lakshman <lakshmanmurugesan@gmail.com>
2024-04-14 23:55:04 +0530

    Update secrets

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 const config = {
-  RECIPE_API_KEY: 'a6b0d31b49844233ae2bc10277c321cf',
-  DD_APP_ID: '4073d1f0-a0c4-448e-94bc-3a0a18613faf',
-  DD_CLIENT_TOKEN: 'pubfbf4e19a6484bfd6716dcf8033d9c4b8',
-  ENVIRONMENT: 'production'
+  RECIPE_API_KEY: process.env.REACT_APP_RECIPE_API_KEY,
+  DD_APP_ID: process.env.REACT_APP_DD_APP_ID,
+  DD_CLIENT_TOKEN: process.env.REACT_APP_DD_CLIENT_TOKEN,
+  ENVIRONMENT: process.env.REACT_APP_ENVIRONMENT
 };
 
 export default config;
